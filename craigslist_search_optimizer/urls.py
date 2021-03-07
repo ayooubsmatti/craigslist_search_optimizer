3712fffbf408cdb8829e24f85d7fd7b82fce752f
ayooubsmatti <ayooubsmatti@gmail.com>
2021-03-07 03:20:18 +0100

    version with some design and link source code with static files

--- craigslist_search_optimizer/urls.py
+++ craigslist_search_optimizer/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from my_app import views
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from my_app import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', include('my_app.urls')),
     path('admin/', admin.site.urls),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request,'my_app/base.html')
+    return render(request,'base.html')
+
+def new_search(request):
+    return render(request,'my_app/new_search.html')
