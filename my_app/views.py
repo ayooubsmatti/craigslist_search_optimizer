c5dadf3d0d738cb0a3e4f15968bc91bbd5ef9e87
ayooubsmatti <ayooubsmatti@gmail.com>
2021-03-08 00:32:23 +0100

    frontend with requirement file

---
+++ my_app/views.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+from django.shortcuts import render
+from requests.compat import quote_plus
+from . import models
+
+
+def home(request):
+    return render(request,'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    print(search)
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(request,'my_app/new_search.html',stuff_for_frontend)
